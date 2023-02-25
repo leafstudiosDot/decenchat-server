0a8516a310cdf5bba3d0beb67485ca161895a22f
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-02-25 08:30:33 +0800

    Icon Route

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, post, middleware, web, App, HttpRequest, HttpServer, Result, Responder};
+use actix_service::{ServiceFactory, IntoServiceFactory};
+use actix_http::Response;
+use actix_web::{get, post, middleware, web, App, HttpRequest, HttpServer, Result, HttpResponse, Error, Responder};
+use actix_files::NamedFile;
+use std::path::PathBuf;
 use serde::Serialize;
 
 #[derive(Serialize)]
@@ -13,6 +17,12 @@ async fn index() -> impl Responder {
     return web::Json(res)
 }
 
+#[get("/icon.png")]
+async fn icon(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "./assets/icon.png".parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -22,10 +32,12 @@ async fn main() -> std::io::Result<()> {
     println!("Your Decensha Server port for development is 7810");
 
     HttpServer::new(|| {
-        App::new()
+        let app = App::new()
             // enable logger
             .wrap(middleware::Logger::default())
             .service(index)
+            .service(icon);
+        app
     })
     .bind(("0.0.0.0", 7810))?
     .run()
