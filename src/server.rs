c9d88258b732de2be30c7cf1184aaed4f2997913
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2023-08-01 14:23:01 +0800

    More Output for Server Details Route

--- src/server.rs
+++ src/server.rs
@@ -8,13 +8,18 @@ pub async fn details() -> HttpResponse {
 
     let servdetfile = "./assets/details.json".to_string();
 
-    let mut server_details = {
+    let server_details = {
         let text = std::fs::read_to_string(&servdetfile).unwrap();
         serde_json::from_str::<Value>(&text).unwrap()
     };
 
     let res = json!({
         "title": server_details["title"].as_str(),
+        "serverid": server_details["serverid"].as_str(),
+        "signinmethod": server_details["serverid"].as_str(),
+        "nsfwfocused": server_details["nsfwfocused"].as_bool(),
+        "connectionmethod": server_details["connectaccountmethod"].as_array().unwrap(),
+        "serverversion": server_details["serverversion"].to_string().parse::<i32>().unwrap(),
     });
 
     HttpResponse::Ok().json(res)
